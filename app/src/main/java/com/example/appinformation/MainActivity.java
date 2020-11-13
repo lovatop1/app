d2ee4a89dc07a972aa560526881b469d2e8e4e30
lovatop1 <lovatopaola98@gmail.com>
2020-11-13 08:57:36 -0500

    bugs

--- app/src/main/java/com/example/appinformation/MainActivity.java
+++ app/src/main/java/com/example/appinformation/MainActivity.java
@@ -1,18 +1,14 @@
 package com.example.appinformation;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import android.os.AsyncTask;
-import android.content.Intent;
-
-import java.util.Iterator;
 import java.util.List;
 
 import static java.lang.System.out;
@@ -24,68 +20,29 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getAppInfo app1;
-        app1 = new getAppInfo();
-        app1.doInBackground();
-        app1.execute();
-        out.print(app1);
-    }
-
-        public final static String GOOGLE_URL = "https://play.google.com/store/apps/details?id=";
-        public static final String ERROR = "error";
-
-        public class getAppInfo extends AsyncTask<String, String, String> {
-
-
-            private final String TAG = MainActivity.getAppInfo.class.getSimpleName();
-            protected PackageManager pm;
-            protected String result;
-            //private ActivityUtil mActivityUtil;
-
-            @Override
-            protected void onPreExecute() {
-                super.onPreExecute();
-                System.out.println("Pre Executing program");
-            }
-
-            @Override
-            protected String doInBackground(String... errors) {
-                String category;
-                String appName;
-                String result = "";
-                List<ApplicationInfo> packages = pm.getInstalledApplications(PackageManager.GET_META_DATA);
-                Iterator<ApplicationInfo> iterator = packages.iterator();
-                while (iterator.hasNext()) {
-                    ApplicationInfo packageInfo = iterator.next();
-                    appName = (String) pm.getApplicationLabel(packageInfo);
-                    String query_url = GOOGLE_URL + packageInfo.packageName;
-                    category = getCategory(query_url);
-                    Log.i(TAG, query_url);
-                    result= "App: " + appName + "Category:" + category;
-//                    for (int i = 0; i<= packages.size(); i++) {
-//                        if (appName == packageInfo.packageName)
-//                            result="App: " + appName + "Category:" + category;
-//                    }
-                }
-                return result;
-            }
-
-            @Override
-            protected void onPostExecute(String result) {
-                out.print(result);
-            }
-
-            protected String getCategory(String query_url) {
-                try {
-                    Document doc = Jsoup.connect(query_url).get();
-                    Element link = doc.select("span[itemprop=genre]").first();
-                    return link.text();
-                } catch (Exception e) {
-                    return ERROR;
-                }
-            }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            getAppInfo();
         }
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void getAppInfo() {
+        Context context= null;
+        List<PackageInfo> packageList = getPackageManager().getInstalledPackages(0);
+        for (int i=0; i<packageList.size(); i++){
+            PackageInfo info= packageList.get(i);
+            if ((info.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM)==0){
+                String name = info.applicationInfo.loadLabel(getPackageManager()).toString();
+                int category = getApplicationInfo().category;
+                String appCategory = (String) ApplicationInfo.getCategoryTitle(context, category);
+                System.out.println("Name: " + name + "Category: " + appCategory);
+            }
+
+        }
+
+
+    }
+}
 
 
 
